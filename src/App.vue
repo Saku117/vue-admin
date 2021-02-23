2c890d2174cccaee2a26f0ea33f5907bdc68fd34
e <675728138@qq.com>
2021-02-23 17:25:10 +0800

    [ADD] 添加Vue3默认配置

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+  // import Todos from "./views/Todos.vue";
+  export default {
+    name: "App",
+    components: {
+      // Todos,
+    },
+  };
+</script>
+<style>
+  body,
+  html {
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+//动态路由
+
+
+export default router
